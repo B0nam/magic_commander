330e6f57951218f507300d68096190e81db18dc2
B0nam <daniiel.bonam@gmail.com>
2024-08-24 00:20:46 -0300

    Routes definition

--- lib/magic_commander_web/router.ex
+++ lib/magic_commander_web/router.ex
@@ -5,7 +5,26 @@ defmodule MagicCommanderWeb.Router do
     plug :accepts, ["json"]
   end
 
+  scope "/", MagicCommanderWeb do
+    get "/", DefaultController, :index
+  end
+
   scope "/api", MagicCommanderWeb do
     pipe_through :api
+
+    get "/cards", CardController, :index
+    get "/cards/:id", CardController, :show
+    post "/cards", CardController, :create
+    get "/cards/find/:name", CardController, :find
+
+    get "/decks", DeckCardController, :index
+    get "/decks/:id", DeckCardController, :show
+    post "/decks", DeckCardController, :create
+    delete "/decks", DeckCardController, :delete
+
+    post "/decks/:id/populate", DeckCardController, :populate
+    post "/decks/:id/import", DeckCardController, :import
+    get "/decks/:id/export", DeckCardController, :expor
+
   end
 end
